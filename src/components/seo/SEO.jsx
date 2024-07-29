a44a09f3ad3ab5b843fd21abe91a5acf743b201e
EI CHANUDOM <chanudomei122@gmail.com>
2024-07-30 00:12:45 +0700

    m

--- src/components/seo/SEO.jsx
+++ src/components/seo/SEO.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
 
-const SEO = ({ title, description, keywords, url, image }) => {  
+const SEO = ({ title, description, keywords, url, image }) => {
   return (
     <Helmet>
-      <meta property="og:image" content="https://popupsmart.com/blog/user/pages/365.free-keyword-research-tools/Free-keyword-research-tools-cover.png"/>
-
       <title>{title}</title>
       <meta name="description" content={description} />
       <meta name="keywords" content={keywords} />
@@ -13,9 +11,9 @@ const SEO = ({ title, description, keywords, url, image }) => {
       <meta property="og:description" content={description} />
       <meta property="og:type" content="website" />
       <meta property="og:url" content={url} />
-      <meta property="og:image:width" content="1200"/>
-      <meta property="og:image:height" content="630"/>
-      {/* <meta property="og:image" content={image} /> */}
+      <meta property="og:image" content={image} />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
     </Helmet>
   );
 };
