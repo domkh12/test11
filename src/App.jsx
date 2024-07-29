ec3930a3ab0e33fcb0f448a3b4fc0b8765b709ba
EI CHANUDOM <chanudomei122@gmail.com>
2024-07-29 21:22:07 +0700

    p

--- src/App.jsx
+++ src/App.jsx
@@ -12,13 +12,15 @@ import { HelmetProvider } from "react-helmet-async";
 import SEO from "./components/seo/SEO";
 import ScrollToTopButton from "./components/homepage/ScrollToTopButton";
 function App() {
+  const url = "https://test11-ecru.vercel.app/";
   return (
+    
     <HelmetProvider>
       <SEO
         title="Showcase - Your Portfolio Builder"
         description="Build and customize your portfolio with ease using our portfolio builder."
         keywords="portfolio, builder, templates, showcase"
-        url="https://5c74-175-100-10-208.ngrok-free.app/"
+        url={url}
         image="/imgSeo.png"
       />
 
